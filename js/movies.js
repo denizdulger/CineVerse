04a119ccb0bddc36a9c883bb7d5221c2267d9efe
gorkemkaramolla <gorkemkaramolla@gmail.com>
2026-07-27 11:45:38 +0300

    chore: change style on movie-info class. I added display flex class to show rating and title in a single row

--- js/movies.js
+++ js/movies.js
@@ -1,35 +1,38 @@
 const baseUrl = "https://image.tmdb.org/t/p/w440_and_h660_face";
 
 async function getMovies() {
-  const response = await fetch("https://api.themoviedb.org/3/discover/movie", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMGE3ZTNhOGQ3ZjdkNzNiZGNkYzQxZmE1ZmFlY2RiZiIsIm5iZiI6MTc4NDYxODQ5OC40LCJzdWIiOiI2YTVmMWUwMjgyOTdhMWQwZjEzMzM0YTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.w6etp5xgPP7C3aO0J4Udcs05_Lo0T6gw8UswFkGZboI",
-    },
-  });
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch(
+        "https://api.themoviedb.org/3/discover/movie",
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMGE3ZTNhOGQ3ZjdkNzNiZGNkYzQxZmE1ZmFlY2RiZiIsIm5iZiI6MTc4NDYxODQ5OC40LCJzdWIiOiI2YTVmMWUwMjgyOTdhMWQwZjEzMzM0YTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.w6etp5xgPP7C3aO0J4Udcs05_Lo0T6gw8UswFkGZboI",
+            },
+        },
+    );
+    const data = await response.json();
+    console.log(data);
 
-  //MOvies div diye değişken oluşturuldu. İçine document.getelementbyid ile movies idli divi seçtik ve değişkene eşitledik
-  let moviesDiv = document.getElementById("movies");
-  //movies divini gittik innerHTML yani normal şartlarda <div id="movies">innerHTML </div>
+    //MOvies div diye değişken oluşturuldu. İçine document.getelementbyid ile movies idli divi seçtik ve değişkene eşitledik
+    let moviesDiv = document.getElementById("movies");
+    //movies divini gittik innerHTML yani normal şartlarda <div id="movies">innerHTML </div>
 
-  for (let i = 0; i < data.results.length; i++) {
-    if (data.results[i].id.toString() === "261639") {
-      continue;
-    }
-    console.log(data.results[i].title);
-    console.log("Film ID:", data.results[i].id);
-    console.log(`movie.html?id=${data.results[i].id}`);
-    moviesDiv.innerHTML += `
+    for (let i = 0; i < data.results.length; i++) {
+        if (data.results[i].id.toString() === "261639") {
+            continue;
+        }
+        console.log(data.results[i].title);
+        console.log("Film ID:", data.results[i].id);
+        console.log(`movie.html?id=${data.results[i].id}`);
+        moviesDiv.innerHTML += `
     <a class="movie-card" href="movie.html?id=${data.results[i].id}">
       <img class='poster' src='${
-        baseUrl + data["results"][i]["poster_path"]
+          baseUrl + data["results"][i]["poster_path"]
       }'/> 
       <div class="movie-description">
-         <div class="movi e-info">
+         <div class="movie-info">
           <h2 class="title">${data.results[i].title}</h2>
 
           <p class="rating">
@@ -43,8 +46,8 @@ async function getMovies() {
       </div>
    </a>
     `;
-  }
-  console.log(moviesDiv);
+    }
+    console.log(moviesDiv);
 }
 
 getMovies();
